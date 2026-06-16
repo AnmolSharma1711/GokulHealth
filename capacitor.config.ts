4f16d2490a5b73ab235bdbb5405144635551a9d2
Anmol Sharma <AnmolSharma1711@users.noreply.github.com>
2026-06-16 13:20:40 +0530

    Rename app to JanSahayak and update App Icon

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'com.gokul.janshayak',
-  appName: 'Booking Ecosystem',
+  appName: 'JanSahayak',
   webDir: 'dist'
 };
 
